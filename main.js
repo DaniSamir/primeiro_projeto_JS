e2241576154340e29edd83fdb6d7a1f5d6194794
DaniSamir <daniella2309@hotmail.com>
2024-04-11 20:50:55 -0300

    finish project

---
+++ main.js
@@ -0,0 +1,40 @@
+// Boas práticas em JS manter um arquivo especifico para funções JS
+
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+//Para guardamos a função dentro de um atributo precisamos retirar os parenteses. 
+
+
+
+function tocaSom (idElementoAudio) {
+   if(elemento === null){
+    alert('Elemento não encontrado');
+   }
+   else{
+    document.querySelector(idElementoAudio).play();
+   }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento ){
+        if(evento.code === 'Space' || evento.code ==='Enter'){    //=== em java compara valor e tipo do dado
+            tecla.classList.add('ativa');
+        }
+    
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+
+}
